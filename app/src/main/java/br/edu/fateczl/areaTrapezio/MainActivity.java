ce191432f6b5a63f10237d29f7505d11dbdb70c5
leudvanjr <leudvanjuan87@gmail.com>
2024-10-03 03:42:54 -0300

    Ajustando a main activity

--- app/src/main/java/br/edu/fateczl/areaTrapezio/MainActivity.java
+++ app/src/main/java/br/edu/fateczl/areaTrapezio/MainActivity.java
@@ -1,6 +1,13 @@
+/*
+ *@author:<Leudvan Guedes>
+ */
+
 package br.edu.fateczl.areaTrapezio;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +16,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etBaseMaior;
+    private EditText etBaseMenor;
+    private EditText etAltura;
+    private TextView tvResposta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        etBaseMaior = findViewById(R.id.etBaseMaior);
+        etBaseMenor = findViewById(R.id.etBaseMenor);
+        etAltura = findViewById(R.id.etAltura);
+        tvResposta = findViewById(R.id.tvResposta);
+        Button btnCalcular = findViewById(R.id.btnCalcular);
+
+        btnCalcular.setOnClickListener(op -> calcular());
     }
+
+    private void calcular() {
+        int baseMaior = Integer.parseInt(etBaseMaior.getText().toString());
+        int baseMenor = Integer.parseInt(etBaseMenor.getText().toString());
+        int altura = Integer.parseInt(etAltura.getText().toString());
+        limparEditTexts(etBaseMaior,etBaseMenor,etAltura);
+
+        int areaTrapezio = ((baseMaior+baseMenor)*altura)/2;
+        String res = getString(R.string.resposta) + " " + areaTrapezio + "m";
+
+        tvResposta.setText(res);
+    }
+
+    private void limparEditTexts(EditText... et) {
+        for (EditText editText: et) {
+            editText.setText("");
+        }
+    }
+
 }
